291319a30938f5aa699871451ae7b8bf5014c015
Chad Harris <chadharrishq@gmail.com>
2014-03-24 21:37:33 +1100

    Prep for version 3 - Replace the ns inline.

--- src/lt/plugins/slamhound_lt.cljs
+++ src/lt/plugins/slamhound_lt.cljs
@@ -3,31 +3,73 @@
             [lt.objs.editor :as editor]
             [lt.objs.editor.pool :as pool]
             [lt.objs.notifos :as notifos]
-            [lt.objs.command :as cmd])
+            [lt.objs.command :as cmd]
+            [lt.plugins.paredit :as par])
   (:require-macros [lt.macros :refer [defui behavior]]))
 
-(defn slamhound-expression [path] (str "(do (require 'slam.hound)(slam.hound/reconstruct \"" path "\"))" ))
+(defn sl-str
+  [path]
+  (str     "(do (require 'slam.hound)"
+           "(binding [clojure.pprint/*print-right-margin* 90]"
+           "(let [file (java.io.File. \"" path "\")]"
+           "(slam.hound/swap-in-reconstructed-ns-form file))))"))
 
 (cmd/command {:command ::slamhound
-              :desc "slamhound-lt: Preview"
+              :desc "slamhound-lt: Replace"
               :exec (fn []
+
                       (when-let [ed (pool/last-active)]
                         (let [path (:path (:info @ed))
-                              exp (slamhound-expression path)]
-                          (notifos/set-msg! (str "Running slamhound on " exp))
+                              exp (sl-str path)]
 
-                          (let [cur (pool/last-active)]
-                            (editor/move-cursor cur {:ch 0
-                                                     :line 0}))
-
+                          (notifos/set-msg! (str "Running slamhound on " path))
                           (object/raise ed
                                         :eval.custom
                                         exp
-                                        {:result-type :inline :verbatim true}
-                                        )
+                                        {:result-type :inline :verbatim true}))))})
 
-                          (notifos/set-msg! "Slamhound done."))))})
 
-;; (defui doc-ui [result]
-;;   [:div.inline-doc
-;;    [:h1 "SlAmHoUnD"]])
+;; (cmd/command {:command ::slamhound-preview
+;;               :desc "slamhound-lt: Preview"
+;;               :exec (fn []
+;;                       (when-let [ed (pool/last-active)]
+;;                         (let [path (:path (:info @ed))
+;;                               exp (slamhound-expression path)]
+
+
+;;                           (let [cur (pool/last-active)]
+;;                             (editor/move-cursor cur {:ch 0
+;;                                                      :line 0}))
+
+;;                           (object/raise ed
+;;                                         :eval.custom
+;;                                         exp
+;;                                         {:result-type :inline :verbatim true}
+;;                                         ))))})
+
+;; (cmd/command {:command ::slamhound-re
+;;               :desc "slamhound-lt: Recursive Select"
+;;               :exec (fn [] (when-let [ed (pool/last-active)]
+;;                         (let [path (:path (:info @ed))
+;;                               exp (slamhound-expression path)]
+
+;;                           (cmd/exec! :paredit.select.parent)
+;;                           )))})
+
+
+;; (defn me-expression [exp] (str "(macroexpand '" exp ")" ))
+
+;; (cmd/command {:command ::macro-expand
+;;               :desc "macro-expand: Expand"
+;;               :exec (fn [] (when-let [ed (pool/last-active)]
+;;                              (cmd/exec! :paredit.select.parent)
+;;                              (let [path (:path (:info @ed))
+;;                                    exp (me-expression (editor/selection ed))]
+
+;;                                (notifos/set-msg! (str "Running slamhound on " exp))
+
+
+;;                                (object/raise ed
+;;                                              :eval.custom
+;;                                              exp
+;;                                              {:result-type :inline :verbatim true}))))})
