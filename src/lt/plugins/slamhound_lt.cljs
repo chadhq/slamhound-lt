0e1793305e2472ce71666e2d145f2a57ddccc41e
Chad Harris <chadharrishq@gmail.com>
2014-03-24 21:49:22 +1100

    Cleanup for version 0.0.3 - Replace the ns inline.

--- src/lt/plugins/slamhound_lt.cljs
+++ src/lt/plugins/slamhound_lt.cljs
@@ -3,73 +3,25 @@
             [lt.objs.editor :as editor]
             [lt.objs.editor.pool :as pool]
             [lt.objs.notifos :as notifos]
-            [lt.objs.command :as cmd]
-            [lt.plugins.paredit :as par])
+            [lt.objs.command :as cmd])
   (:require-macros [lt.macros :refer [defui behavior]]))
 
-(defn sl-str
+(defn slamhound-str
   [path]
-  (str     "(do (require 'slam.hound)"
-           "(binding [clojure.pprint/*print-right-margin* 90]"
-           "(let [file (java.io.File. \"" path "\")]"
-           "(slam.hound/swap-in-reconstructed-ns-form file))))"))
+  (str "(do (require 'slam.hound)"
+       "(binding [clojure.pprint/*print-right-margin* 80]"
+       "(let [file (java.io.File. \"" path "\")]"
+       "(slam.hound/swap-in-reconstructed-ns-form file))))"))
 
 (cmd/command {:command ::slamhound
-              :desc "slamhound-lt: Replace"
+              :desc "slamhound-lt: Reconstruct namespace"
               :exec (fn []
-
                       (when-let [ed (pool/last-active)]
                         (let [path (:path (:info @ed))
-                              exp (sl-str path)]
+                              exp (slamhound-str path)]
 
                           (notifos/set-msg! (str "Running slamhound on " path))
                           (object/raise ed
                                         :eval.custom
                                         exp
                                         {:result-type :inline :verbatim true}))))})
-
-
-;; (cmd/command {:command ::slamhound-preview
-;;               :desc "slamhound-lt: Preview"
-;;               :exec (fn []
-;;                       (when-let [ed (pool/last-active)]
-;;                         (let [path (:path (:info @ed))
-;;                               exp (slamhound-expression path)]
-
-
-;;                           (let [cur (pool/last-active)]
-;;                             (editor/move-cursor cur {:ch 0
-;;                                                      :line 0}))
-
-;;                           (object/raise ed
-;;                                         :eval.custom
-;;                                         exp
-;;                                         {:result-type :inline :verbatim true}
-;;                                         ))))})
-
-;; (cmd/command {:command ::slamhound-re
-;;               :desc "slamhound-lt: Recursive Select"
-;;               :exec (fn [] (when-let [ed (pool/last-active)]
-;;                         (let [path (:path (:info @ed))
-;;                               exp (slamhound-expression path)]
-
-;;                           (cmd/exec! :paredit.select.parent)
-;;                           )))})
-
-
-;; (defn me-expression [exp] (str "(macroexpand '" exp ")" ))
-
-;; (cmd/command {:command ::macro-expand
-;;               :desc "macro-expand: Expand"
-;;               :exec (fn [] (when-let [ed (pool/last-active)]
-;;                              (cmd/exec! :paredit.select.parent)
-;;                              (let [path (:path (:info @ed))
-;;                                    exp (me-expression (editor/selection ed))]
-
-;;                                (notifos/set-msg! (str "Running slamhound on " exp))
-
-
-;;                                (object/raise ed
-;;                                              :eval.custom
-;;                                              exp
-;;                                              {:result-type :inline :verbatim true}))))})
